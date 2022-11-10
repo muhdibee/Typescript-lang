7a0e45ac5c4589d6d62e46e694aa4d1be5ab562e
Muhammad <muhdibee@gmail.com>
2022-11-10 21:20:02 +0100

    TypScript built-in types.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const sum: Function = (a: number, b: number)=>{
+    console.log(2+b);
+}
+
+// sum(5+5);
+
+
+// Data types. Every anotation of available begins with a small letter e.g "string" is uses to anotate a String type variable.
+let numbers: number[] = [1, 2, 3];  // number array.
+let a: number = 1_034_574_568;  // Number.  Underscores are placed for better readability.
+let firstName: string = "Muhammad"; // String.
+let is_published: boolean = true;  // Boolean
+let myDocument; // Any.  when a variable is not initialized, the type becomes any.
