12d314dae8a22a131c95069fd6f089dc5ee0c2d7
Muhammad <muhdibee@gmail.com>
2022-11-15 08:30:09 +0100

    Turple and enum.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 const sum: Function = (a: number, b: number)=>{
-    console.log(2+b);
+    console.log(`${a} + ${b} is ${a+b}.`);
 }
 
-// sum(5+5);
+sum(5,5);
 
 
 // Data types. Every anotation of available begins with a small letter e.g "string" is uses to anotate a String type variable.
@@ -11,3 +11,12 @@ let a: number = 1_034_574_568;  // Number.  Underscores are placed for better re
 let firstName: string = "Muhammad"; // String.
 let is_published: boolean = true;  // Boolean
 let myDocument; // Any.  when a variable is not initialized, the type becomes any.
+
+// Tuple
+let user: [number, string] = [20, "Muhammad"]; // tuple type with specific type for each element.
+console.log(`${user[1]} ${user[0]}`);
+
+// enum
+const enum  Size {Small = 1,  medium, large, xLarge};
+let mySize = Size.xLarge;
+console.log(mySize)
