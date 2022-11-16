32116a8cf4652cd2f7f96c45413edb2683541f4f
Muhammad <muhdibee@gmail.com>
2022-11-16 11:07:19 +0100

    Function types, parameters and return types.

--- Hello-world/index.ts
+++
@@ -1,5 +0,0 @@
-const sum: Function = (a: number, b: number)=>{
-    console.log(2+b);
-}
-
-sum(5+5);
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const sum: Function = (a: number, b: number)=>{
 sum(5,5);
 
 
-// Data types. Every anotation of available begins with a small letter e.g "string" is uses to anotate a String type variable.
+// Data types. Every annotation of available begins with a small letter e.g "string" is uses to annotate a String type variable.
 let numbers: number[] = [1, 2, 3];  // number array.
 let a: number = 1_034_574_568;  // Number.  Underscores are placed for better readability.
 let firstName: string = "Muhammad"; // String.
@@ -16,7 +16,18 @@ let myDocument; // Any.  when a variable is not initialized, the type becomes an
 let user: [number, string] = [20, "Muhammad"]; // tuple type with specific type for each element.
 console.log(`${user[1]} ${user[0]}`);
 
-// enum
-const enum  Size {Small = 1,  medium, large, xLarge};
-let mySize = Size.xLarge;
+// enum: enums are used to set multiple constant variables. first value can be set and the rest set automatically.
+const enum  Size {Small = 1,  Medium, Large, XLarge};
+let mySize = Size.XLarge;
 console.log(mySize)
+
+
+// functions: functions should be annotated with a return type and parameters can have default values.
+function calculateTax(income: number, taxYear: number = 2022): number {
+    if(taxYear < 2022){
+        return income * 0.1
+    }
+    return income * 0.15
+}
+
+console.log(calculateTax(50_000))
